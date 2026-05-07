77c9ecb91429acdae25c93fd5c4162e099ee0fe5
Redwan <redwanhasan980@gmail.com>
2026-05-08 03:16:34 +0600

    Selenium Sauce test

--- src/test/java/SauceTest.java
+++ src/test/java/SauceTest.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -13,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class SauceTestByEmonSir {
+public class SauceTest {
     private WebDriver driver;
     private WebDriverWait wait;
     private String url ="https://www.saucedemo.com/";
@@ -22,13 +24,15 @@ public class SauceTestByEmonSir {
     @BeforeEach
     void setup()
     {
-         driver = new ChromeDriver();
+         driver = new FirefoxDriver();
+        driver.manage().window().maximize();
          driver.get(url);
          wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @AfterEach
-    void tearDown() throws InterruptedException { Thread.sleep(1000);
+    void tearDown() throws InterruptedException {
+        Thread.sleep(1000);
          driver.quit();
     }
 
@@ -40,7 +44,7 @@ public class SauceTestByEmonSir {
         password.sendKeys(pass);
         Thread.sleep(1000);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login-button"))).click();
-
+        Thread.sleep(1000);
     }
     @Order(1)
     @DisplayName("Successful Login Test")
@@ -103,15 +107,89 @@ public class SauceTestByEmonSir {
     @Test
     void logOut() throws InterruptedException {
         login("standard_user","secret_sauce");
-        Thread.sleep(1000);
+
         assertTrue(driver.getCurrentUrl().contains("inventory"),"Should redirect to inventory");
         WebElement menu = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("react-burger-menu-btn")));
         menu.click();
-        Thread.sleep(1000);
-//        wait.until(ExpectedConditions.elementToBeClickable(By.id("logout_sidebar_link"))).click();
+        //Thread.sleep(5000);
+    wait.until(ExpectedConditions.elementToBeClickable(By.id("logout_sidebar_link"))).click();
        // driver.findElement(By.id("logout_sidebar_link")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("logout_sidebar_link"))).click();
-        Thread.sleep(2000);
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("logout_sidebar_link"))).click();
+        //Thread.sleep(2000);
         assertEquals("https://www.saucedemo.com/",driver.getCurrentUrl(),"should be in the home page");
+    }
+    @Order(7)
+    @DisplayName("Test Checkout Process")
+    @Test
+    void testCheckout( )throws InterruptedException {
+        login("standard_user","secret_sauce");
+        Thread.sleep(1000);
+        addItemtoCart(0);
+        Thread.sleep(1000);
+        addItemtoCart(1);
+        Thread.sleep(1000);
+        WebElement cartIcon = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".shopping_cart_link")));
+        cartIcon.click();
+        Thread.sleep(1000);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("checkout"))).click();
+        Thread.sleep(1000);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("first-name"))).sendKeys("Redwan");
+          wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("last-name"))).sendKeys("hasan");
+          wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("postal-code"))).sendKeys("1380");
+        Thread.sleep(1000);
+          wait.until(ExpectedConditions.elementToBeClickable(By.id("continue"))).click();
+        Thread.sleep(1000);
+   wait.until(ExpectedConditions.elementToBeClickable(By.id("finish"))).click();
+   WebElement text = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".complete-header")));
+   assertEquals("Thank you for your order!",text.getText(),"Should be there");
+    }
+    @Order(8)
+    @DisplayName("Test Checkout Field Empty")
+    @Test
+    void testCheckoutEmpty() throws InterruptedException {
+        login("standard_user","secret_sauce");
+        Thread.sleep(1000);
+        addItemtoCart(0);
+        Thread.sleep(1000);
+        addItemtoCart(1);
+        Thread.sleep(1000);
+        WebElement cartIcon = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".shopping_cart_link")));
+        cartIcon.click();
+        Thread.sleep(1000);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("checkout"))).click();
+        Thread.sleep(1000);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("first-name"))).clear();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("last-name"))).sendKeys("hasan");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("postal-code"))).sendKeys("1380");
+        Thread.sleep(1000);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("continue"))).click();
+        Thread.sleep(1000);
+        WebElement msg = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-test='error']")));
+        assertTrue(msg.getText().contains("Error: First Name is required"),"Should throw error");
+    }
+
+    @Order(8)
+    @DisplayName("Using Xpath")
+    @Test
+    void xpathTest() throws InterruptedException {
+        login("standard_user","secret_sauce");
+        //find by xpath
+        WebElement product = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
+                "//div[text()='Sauce Labs Backpack']")));
+        assertTrue(product.isDisplayed(),"Not available");
+        WebElement addToCart =driver.findElement(By.xpath
+                ("//div[text()='Sauce Labs Backpack']" +
+                        "/ancestor::div[@class='inventory_item']"+
+                        "//button"
+                ));
+         addToCart.click();
+         WebElement badge =driver.findElement(By.xpath("//span[@class='shopping_cart_badge']"));
+         assertEquals("1",badge.getText());
+    }
+    void addItemtoCart(int index)
+    {
+       List<WebElement> addButton = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(".btn_inventory")));
+       assertTrue(addButton.size()>0,"Should have item");
+       addButton.get(index).click();
     }
 }
